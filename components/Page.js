a4d64b92a5bce4cc6c94843e06a0e5591c454510
Conner Matson <cnr.mtsn@gmail.com>
2021-06-26 08:50:14 -0500

    favicon android fix

--- components/Page.js
+++ components/Page.js
@@ -18,12 +18,12 @@ export default function Page({ children }) {
 					Conner Matson | {path}
 				</title>
 				<meta name="description" content="My personal development portfolio" />
-				<link rel="icon" href="favicon.ico" />
-
-				<link rel="icon" href="/favicon.ico"/>
-				<link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
-
-
+				<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"/>
+				<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
+				<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192"/>
+				<link rel="icon" type="image/png" href="/android-chrome-512x512.png" sizes="512x512"/>
+				<link rel="shortcut icon" href="/favicon.ico"/>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet" />
