618c742777b9c986ecfcbe4b4ddd69c2cb154133
Conner Matson <cnr.mtsn@gmail.com>
2021-06-26 11:46:03 -0500

    changed footer to github logo -opaque

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import styles from '../styles/Footer.module.css'
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
+
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
 			<a href="https://github.com/cnr-mtsn">
-				Conner Matson | <span id="github-logo">🖥️</span>
+				<Image height="40" width="40" src="/githubLight.png" />
 			</a>
 		</footer>
 	);
